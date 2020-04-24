c8e560e9c0cf1b294ffbd3908b5ddccff4388db6
Ozhan Yilmaz <ozhanyilmaz@gmail.com>
2020-04-24 18:26:40 +0200

    initial socket messages

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 
+const { addUser, getUser, getUsersInRoom, removeUser } = require("./users");
 const PORT = process.env.port || 5000;
 
 const router = require("./router");
@@ -11,14 +12,32 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on("connection", (socket) => {
-  console.log("a user is connected!!!");
+  //console.log("a user is connected!!!");
 
   socket.on("join", ({ name, room }, callback) => {
-    const error = true;
+    const { error, user } = addUser({ id: socket.id, name, room });
 
-    if (error) {
-      callback({ error: "error" });
-    }
+    if (error) return callback(error);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name} has joined!` });
+
+    socket.join(user.room);
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback();
   });
 
   socket.on("disconnected", () => {
